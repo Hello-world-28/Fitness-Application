97d966ca16c6ac7b577750df930f71a66394452a
Saurabh <suarbhbora7060@gmail.com>
2026-06-09 11:44:20 +0530

    User Entity + Docker-compose

---
+++ userservice/src/main/java/com/fitness/userservice/model/User.java
@@ -0,0 +1,34 @@
+package com.fitness.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String firstName;
+    private String lastName;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
